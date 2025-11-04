89e810389f874ce9dec576431245cf6cd8110ada
Tommimimimi <palmertommy876@gmail.com>
2025-11-04 13:36:56 +0000

    added animation for pacman alongside quality of life changes

--- PacmanIterationOne/Form1.cs
+++ PacmanIterationOne/Form1.cs
@@ -2,11 +2,30 @@ using System.Threading;
 using System;
 using System.Runtime.CompilerServices;
 using System.Numerics;
+using System.Diagnostics;
 
 namespace PacmanIterationOne
 {
+    //define different directions as named constants in enum
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+        None
+    }
+    
     public partial class Form1 : Form
     {
+        public Dictionary<Direction, float> directionAngle = new()
+        {
+            { Direction.Up, 270 },
+            { Direction.Down, 90 },
+            { Direction.Left, 180 },
+            { Direction.Right, 0 },
+            { Direction.None, 0 }
+        };
         //define empty maze
         int[,] arrMaze;
 
@@ -19,26 +38,27 @@ namespace PacmanIterationOne
             intMazeY,
             intCellSize = 40;
 
-        //define different directions as named constants in enum
-        enum Direction
-        {
-            Up,
-            Down,
-            Left,
-            Right,
-            None
-        }
-
         //declare current and next direction variables
-        private static Direction 
-            dirCurrent = Direction.None,
-            dirNext = Direction.None;
+        static Direction 
+        dirCurrent = Direction.None,
+        dirNext = Direction.None;
 
         Random rnd = new Random();
 
         //create system resources
         Thread thrdGameLoop;
         Rectangle rectPlayer;
+
+        List<Ghost> listGhosts = new List<Ghost>();
+
+        int R;
+        int G;
+        int B;
+        Brush brush = new SolidBrush(Color.FromArgb(200, 20, 20, 20));
+
+        float fltMouthAngle = 0;
+
+        Stopwatch swMouthTime = new Stopwatch();
 
         public Form1()
         {
@@ -61,12 +81,17 @@ namespace PacmanIterationOne
             intPlayerSpeed = intCellSize / 8;
             rectPlayer = new Rectangle(intPlayerX, intPlayerY, intCellSize, intCellSize);
 
+            //create the four ghosts
+            listGhosts.Add(new Ghost(400, 400, Color.Red, arrMaze, intCellSize));
+            listGhosts.Add(new Ghost(300, 300, Color.Pink, arrMaze, intCellSize));
+            listGhosts.Add(new Ghost(100, 100, Color.Cyan, arrMaze, intCellSize));
+            listGhosts.Add(new Ghost(200, 200, Color.Orange, arrMaze, intCellSize));
+
+
             //create and start game loop thread
             thrdGameLoop = new Thread(GameLoop);
             thrdGameLoop.Start();
 
-            //attach keydown event handler and add double buffering for smoother rendering
-            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
             this.DoubleBuffered = true;
         }
 
@@ -102,12 +127,143 @@ namespace PacmanIterationOne
             }
         }
 
-        private void MazeCreate()
+        private void MazePathing(int paraRow, int paraCol)
         {
-            //maze generation logic to be implemented
-            SetMazeValue();
+            arrMaze[paraRow, paraCol] = 0;
+
+            List<Direction> mazeDirections = new List<Direction>
+            {
+                Direction.Up, Direction.Down, Direction.Left, Direction.Right
+            };
+
+            for (int i = 0; i < mazeDirections.Count; i++)
+            {
+                int swapIndex = rnd.Next(i, mazeDirections.Count);
+                var temp = mazeDirections[i];
+                mazeDirections[i] = mazeDirections[swapIndex];
+                mazeDirections[swapIndex] = temp;
+            }
+
+            foreach (var direction in mazeDirections)
+            {
+                int testRow = paraRow;
+                int testCol = paraCol;
+
+                switch (direction)
+                {
+                    case Direction.Up:
+                        testRow = paraRow - 2;
+                        break;
+                    case Direction.Down:
+                        testRow = paraRow + 2;
+                        break;
+                    case Direction.Left:
+                        testCol = paraCol - 2;
+                        break;
+                    case Direction.Right:
+                        testCol = paraCol + 2;
+                        break;
+                }
+                if (testRow > 0 && testRow < intMazeX && testCol > 0 && testCol < intMazeY
+                    && arrMaze[testRow, testCol] == 1)
+                {
+                    arrMaze[(paraRow + testRow) / 2, (paraCol + testCol) / 2] = 0;
+                    MazePathing(testRow, testCol);
+                }
+            }
+        }
+        private void DeadEndRemove()
+        {
+            for (int row = 1; row < intMazeX - 1; row++)
+            {
+                for (int col = 1; col < intMazeY - 1; col++)
+                {
+                    int walls = 0;
+                    //stores the cell opposite the single open cell within a deadend
+                    Direction? dirOpenCell = null;
+
+                    if (arrMaze[row - 1, col] == 1) { walls++; }
+                    else { dirOpenCell = Direction.Down; }
+
+                    if (arrMaze[row + 1, col] == 1) { walls++; }
+                    else { dirOpenCell = Direction.Up; }
+
+                    if (arrMaze[row, col - 1] == 1) { walls++; }
+                    else { dirOpenCell = Direction.Right; }
+
+                    if (arrMaze[row, col + 1] == 1) { walls++; }
+                    else { dirOpenCell = Direction.Left; }
+
+                    if (walls == 3)
+                    {
+                        switch (dirOpenCell)
+                        {
+                            case Direction.Left:
+                                arrMaze[row, col - 1] = 0;
+                                break;
+                            case Direction.Right:
+                                arrMaze[row, col + 1] = 0;
+                                break;
+                            case Direction.Up:
+                                arrMaze[row - 1, col] = 0;
+                                break;
+                            case Direction.Down:
+                                arrMaze[row + 1, col] = 0;
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+        private void BoundaryReadd()
+        {
+            for (int row = 0; row < intMazeX; row++)
+            {
+                arrMaze[row, 0] = 1;
+            }
+            for (int col = 0; col < intMazeY; col++)
+            {
+                arrMaze[intMazeX - 1, col] = 1;
+            }
+            for (int col = 0; col < intMazeY; col++)
+            {
+                arrMaze[0, col] = 1;
+            }
+            for (int row = 0; row < intMazeX; row++)
+            {
+                arrMaze[row, intMazeY - 1] = 1;
+            }
         }
 
+        private void PelletAdd()
+        {
+            for (int x = 0; x < intMazeX; x++)
+            {
+                for (int y = 0; y < intMazeY; y++)
+                {
+                    if (arrMaze[x, y] == 0)
+                        arrMaze[x, y] = 2;                 
+                }
+            }
+        }
+
+        private void MazeCreate()
+        {
+            SetMazeValue();
+            MazePathing(1, 1);
+            DeadEndRemove();
+            BoundaryReadd();
+            PelletAdd();
+            RandomBrushColours();
+        }
+
+        private void RandomBrushColours()
+        {
+            R = rnd.Next(150, 220);
+            G = rnd.Next(50, 220);
+            B = rnd.Next(50, 220);
+            brush = new SolidBrush(Color.FromArgb(200, R, G, B));
+        }
 
         protected override void OnPaint(PaintEventArgs e)
         {
@@ -120,19 +276,150 @@ namespace PacmanIterationOne
                 for (int col = 0; col < intMazeY; col++)
                 {
                     if (arrMaze[row, col] == 1)
-                        g.FillRectangle(Brushes.Blue, col * intCellSize, row * intCellSize, intCellSize, intCellSize);
-                    else
+                        g.FillRectangle(brush, col * intCellSize, row * intCellSize, intCellSize, intCellSize);
+
+                    else if (arrMaze[row, col] == 0)
                         g.FillRectangle(Brushes.Black, col * intCellSize, row * intCellSize, intCellSize, intCellSize);
+                    //draw pellets for every 2 in the array
+                    else if (arrMaze[row, col] == 2)
+                    {
+                        //empty space is black rectangle
+                        g.FillRectangle(Brushes.Black, col * intCellSize, row * intCellSize, intCellSize, intCellSize);
+                        //pellet is a small yellow circle in the center of the cell
+                        g.FillEllipse(Brushes.Yellow, 
+                            col * intCellSize + (intCellSize / 5 * 2), row * intCellSize + (intCellSize / 5 * 2), 
+                            intCellSize / 5, intCellSize / 5);
+                    }
                 }
             }
-            //paint player afterwards in order to be on top of maze
-            g.FillRectangle(Brushes.Yellow, rectPlayer);
+
+            //paint player and ghosts afterwards in order to be on top of maze
+            float MouthAngle = (MathF.Sin(fltMouthAngle * 3 + float.Pi / 6) + 0.9f) * 20;
+            g.FillPie(Brushes.Yellow, rectPlayer, directionAngle[dirCurrent] + (MouthAngle), 360 - (2 * MouthAngle));
+            foreach (Ghost ghost in listGhosts)
+            {
+                ghost.Draw(g);
+            }
+        }
+        private void MovePlayer()
+        {
+            //movement attempt variables
+            int tryX = intPlayerX;
+            int tryY = intPlayerY;
+
+            switch (dirNext)
+            {
+                //up and down
+                case Direction.Up:
+                    tryY -= intPlayerSpeed;
+                    break;
+                case Direction.Down:
+                    tryY += intPlayerSpeed;
+                    break;
+                //left and right
+                case Direction.Left:
+                    tryX -= intPlayerSpeed;
+                    break;
+                case Direction.Right:
+                    tryX += intPlayerSpeed;
+                    break;
+            }
+
+            //checks if the new x or y is valid then sets
+            //the players new coordinates and direction
+            if (IsValidMove(tryX, tryY))
+            {
+                intPlayerX = tryX;
+                intPlayerY = tryY;
+                dirCurrent = dirNext;
+                directionAngle[Direction.None] = directionAngle[dirCurrent];
+                if(!swMouthTime.IsRunning)
+                {
+                    swMouthTime.Start();
+                }
+            }
+            else
+            {
+                //reset test variables if they were invalid
+                tryX = intPlayerX;
+                tryY = intPlayerY;
+                //continue going in the current direction 
+                switch (dirCurrent)
+                {
+                    case Direction.Up:
+                        tryY -= intPlayerSpeed;
+                        break;
+                    case Direction.Down:
+                        tryY += intPlayerSpeed;
+                        break;
+                    case Direction.Left:
+                        tryX -= intPlayerSpeed;
+                        break;
+                    case Direction.Right:
+                        tryX += intPlayerSpeed;
+                        break;
+                }
+                //checking for collision on current direction
+                if (IsValidMove(tryX, tryY))
+                {
+                    intPlayerX = tryX;
+                    intPlayerY = tryY;
+                }
+                else
+                {
+                    dirCurrent = Direction.None;
+                    swMouthTime.Reset();
+                }
+            }
+            //paint new player rectangle
+            rectPlayer = new Rectangle(intPlayerX, intPlayerY, intCellSize, intCellSize);
+            //force refresh
+            Invalidate();
+        }
+        
+
+        private bool IsValidMove(int newX, int newY)
+        {
+            //test rectangle for collision
+            Rectangle newPlayerRect = new Rectangle(newX, newY, rectPlayer.Width, rectPlayer.Height);
+
+            //goes through every cell wall and
+            //creates a rectangle for every one
+            for (int row = 0; row < intMazeX; row++)
+            {
+                for (int col = 0; col < intMazeY; col++)
+                {
+                    if (arrMaze[row, col] == 1)
+                    {
+                        Rectangle mazeWall = new Rectangle(col * intCellSize, row * intCellSize, intCellSize, intCellSize);
+                        //using IntersectsWith method to check for collision
+                        //returning IsValidMove as false if the intersect is true
+                        if (newPlayerRect.IntersectsWith(mazeWall))
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            //checks for collision with newX and newY on each side
+            //to make sure player can not go out of bounds at all
+            if (newX < 0 || newY < 0 || newX + rectPlayer.Width > ClientSize.Width || newY + rectPlayer.Height > ClientSize.Height)
+                return false;
+            //if none of the checks are activated
+            //then it is returned as a valid move
+            return true;
         }
 
         private void GameLoop()
         {
             MazeCreate();
-            MessageBox.Show("GameLoop thread is running!");
+            swMouthTime.Start();
+            while (true)
+            {
+                MovePlayer();
+                fltMouthAngle = (float)swMouthTime.Elapsed.TotalSeconds * 7;
+                Thread.Sleep(20);
+            }
         }
     }
 }
---
+++ PacmanIterationOne/Ghost.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacmanIterationOne
+{
+    public class Ghost
+    {
+        public int intX { get; private set; }
+        public int intY { get; private set; }
+        public int intSpeed { get; private set; }
+        public Color colGhost { get; private set; }
+        public Rectangle bounds => new Rectangle(intX, intY, intCellSize, intCellSize);
+        Direction dirGhostCurrent;
+        public enum Phase
+        { 
+            Chase,                          
+            Scatter,                            
+            Frightened
+        }
+        public Phase phaGhostPhase { get; private set; }
+        private int[,] arrMaze;
+        private int intCellSize;
+
+        public Ghost(int paraX, int paraY, Color paraColour, int[,] paraMaze, int paraCellSize)
+        {
+            intX = paraX;
+            intY = paraY;
+            colGhost = paraColour;
+            arrMaze = paraMaze;
+            intCellSize = paraCellSize;
+            dirGhostCurrent = Direction.Left;
+            phaGhostPhase = Phase.Chase;
+        }
+        public void Draw(Graphics g)
+        {
+            g.FillRectangle(new SolidBrush(colGhost), intX, intY, intCellSize, intCellSize);
+        }
+    }
+}
